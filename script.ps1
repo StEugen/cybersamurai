f8e0bd628677a74ead3225fe743130f4c5974a79
StEugen <85479518+StEugen@users.noreply.github.com>
2022-10-27 20:14:27 +0400

    Delete script.ps1

--- script.ps1
+++
@@ -1,9 +0,0 @@
-wget "http://10.0.122.153:82/Win10.ova" -outfile "D:\Win10.ova"
-#Set-ExecutionPolicy unrestricted
-
-cd C:\'Program Files'\Oracle\VirtualBox\
-
-.\VBoxManage.exe unregistervm --delete "Win10"
-.\VBoxManage.exe import D:\Win10.ova 
-
-shutdown /s
