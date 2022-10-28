c832d1fd53f4d568c83a534d475ab4c3e6e295cf
StEugen <85479518+StEugen@users.noreply.github.com>
2022-10-28 10:59:20 +0000

    Update VBoxMachineAuto.ps1

--- Powershell/VBoxMachineAuto.ps1
+++ Powershell/VBoxMachineAuto.ps1
@@ -5,5 +5,5 @@ wget "http://your-server/way/to/machine" -outfile "D:\where\to\put"
 cd C:\way\to\virtualbox\directory\with\VBoxManage.exe
 .\VBoxManage.exe unregistervm --delete "vmname" #if you have this vm installed and want to reput it
 .\VBoxManage.exe import D:\machine_name.ova
-.\VBoxManage.exe modifyvm --memory <amount of RAM in mb> --cpus <number of cpus>
-shutdown /s #optional comment if don't need
+.\VBoxManage.exe modifyvm "VMname" --memory <amount of RAM in mb> --cpus <number of cpus>
+shutdown /s #optional, comment if don't need
