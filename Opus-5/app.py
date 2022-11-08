1315a8823d2f575663bdc6f770bc94ea597d90bf
StEugen <85479518+StEugen@users.noreply.github.com>
2022-11-08 10:13:09 +0000

    added script generator

--- Opus-5/app.py
+++ Opus-5/app.py
@@ -10,8 +10,34 @@ app = Flask(__name__, template_folder='./template')
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == 'POST':
-        print(request.form.getlist('check'))
-        return 'Done'
+        instal = 'sudo apt-get install'
+        f = open('./sh-files/test.sh', 'w')
+        checked =  request.form.getlist('check')
+        print(checked)
+        if "docker" in checked:
+            text = [
+                'sudo apt-get update', 'sudo apt-get install ca-certificates', 
+                'sudo apt-get install curl', 'sudo apt-get install gnupg',
+                'sudo apt-get install lsb-release', 'sudo mkdir -p /etc/apt/keyrings',
+                'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg',
+                'echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu',
+                'echo $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null',
+                'sudo apt-get update', 'sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin', 
+            ]
+            f.write('\n'.join(text))
+            f.write('\n')
+        if "portainer" in checked:
+            text = [
+                'docker volume create portainer_data',
+                'docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest'
+            ]
+            f.write('\n'.join(text))
+            f.write('\n')
+        print('http://')
     return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
 
 
