85b7c173a2e1acc3c4188ecb6565a78fbd9f4d2a
StEugen <85479518+StEugen@users.noreply.github.com>
2022-11-08 11:52:16 +0000

    update

--- Opus-5/app.py
+++ Opus-5/app.py
@@ -47,4 +47,3 @@ def download():
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
 
-
--- Opus-5/sh-files/test.sh
+++ Opus-5/sh-files/test.sh
@@ -1,11 +1,2 @@
-sudo apt-get update
-sudo apt-get install -y ca-certificates
-sudo apt-get install -y curl
-sudo apt-get install -y gnupg
-sudo apt-get install lsb-release
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu
-echo $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
