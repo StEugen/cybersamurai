1c6beb2f55c45b5a7e0d024b98909d01d5c9b98f
StEugen <85479518+StEugen@users.noreply.github.com>
2022-11-08 19:15:47 +0400

    update

--- Opus-5/app.py
+++ Opus-5/app.py
@@ -12,26 +12,28 @@ app = Flask(__name__, template_folder='./template')
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == 'POST':
-        instal = 'sudo apt-get install'
         f = open('./sh-files/test.sh', 'w')
         checked =  request.form.getlist('check')
         print(checked)
-        if "docker" in checked:
+        if "docker-ubuntu-22" in checked:
             text = [
-                'sudo apt-get update', 'sudo apt-get install -y ca-certificates', 
-                'sudo apt-get install -y curl', 'sudo apt-get install -y gnupg',
-                'sudo apt-get install lsb-release', 'sudo mkdir -p /etc/apt/keyrings',
-                'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg',
-                'echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu',
-                'echo $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null',
-                'sudo apt-get update', 'sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin', 
+                '#start of docker installation',
+                'sudo apt-get update',
+                'sudo apt install -y apt-transport-https ca-certificates curl software-properties-common',
+                'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg',
+                'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null',
+                'sudo apt update',
+                'sudo apt install -y docker-ce',
+                '#end of docker installation'
             ]
             f.write('\n'.join(text))
             f.write('\n')
         if "portainer" in checked:
             text = [
+                '#start of portainer in docker installation',
                 'docker volume create portainer_data',
-                'docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest'
+                'docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest',
+                '#end of portainer in docker installation'
             ]
             f.write('\n'.join(text))
             f.write('\n')
