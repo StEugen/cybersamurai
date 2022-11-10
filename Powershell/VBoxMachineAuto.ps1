bbd1e1752c574d4e434c04c663eaf6112e45a9da
StEugen <85479518+StEugen@users.noreply.github.com>
2022-11-10 22:30:09 +0400

    Update VBoxMachineAuto.ps1

--- Powershell/VBoxMachineAuto.ps1
+++ Powershell/VBoxMachineAuto.ps1
@@ -7,3 +7,5 @@ cd C:\way\to\virtualbox\directory\with\VBoxManage.exe
 .\VBoxManage.exe import D:\machine_name.ova
 .\VBoxManage.exe modifyvm "VMname" --memory <amount of RAM in mb> --cpus <number of cpus>
 shutdown /s #optional, comment if don't need
+
+#Well, the story is that once in a while i had to deploy a ton of vms for a university... story to be added
