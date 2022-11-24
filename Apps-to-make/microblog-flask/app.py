1c134f134641e00e15cfabf5fa53d16d95d02e5a
StEugen <85479518+StEugen@users.noreply.github.com>
2022-11-24 20:34:06 +0000

    Update app.py

--- Apps-to-make/microblog-flask/app.py
+++ Apps-to-make/microblog-flask/app.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__, template_folder='./template')
+app.secret_key='change'
+app.config['SQLALCHEMY_DATABASE_URI']=os.getenv('POSTGRESQL_DB')
+db = SQLAlchemy()
+db.init_app(app)
+
+
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30))
+    content = db.Column(db.Text)
 
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    posts = BlogPost.query.order_by(BlogPost.id.desc()).all()
+    return render_template('index.html', posts=posts)
 
 
 @app.route('/post', methods=["POST", "GET"])
