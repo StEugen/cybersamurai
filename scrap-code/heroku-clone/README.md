714da45194d7d0a312f7e5f549185e02635225d7
StEugen <85479518+StEugen@users.noreply.github.com>
2022-11-29 23:09:50 +0400

    update

---
+++ scrap-code/heroku-clone/README.md
@@ -0,0 +1,1 @@
+This is just my heroku clone
--- scrap-code/telegram-ulspu-bot/parser.py
+++ scrap-code/telegram-ulspu-bot/parser.py
@@ -1,10 +1,4 @@
-import requests
- 
-# Making a get request
-response = requests.get('http://192.168.1.8/students/schedule/prepod/')
- 
-# print response
-print(response)
- 
-# print json content
-print(response.json())
+import requests, urllib, json
+
+data = requests.get('https://www.ulspu.ru/students/schedule/prepod/cb4ba39a83855bae2c56ff82453c7511-17.json?0.42403647695275737')
+print(data.json)
