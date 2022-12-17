e74b7fbc42e3cc441580f8650350063097aa78f5
StEugen <85479518+StEugen@users.noreply.github.com>
2022-12-17 12:15:46 +0000

    update

--- scrap-code/telegram-ulspu-bot/parser.py
+++
@@ -1,4 +0,0 @@
-import requests, urllib, json
-
-data = requests.get('https://www.ulspu.ru/students/schedule/prepod/cb4ba39a83855bae2c56ff82453c7511-17.json?0.42403647695275737')
-print(data.json)
--- scrap-code/telegram-ulspu-bot/ulspu_bot.py
+++ scrap-code/telegram-ulspu-bot/ulspu_bot.py
@@ -1,3 +1,17 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types, executor
 from aiohttp import web 
+from config import BOT_API_TOKEN
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
+
+bot = Bot(token=BOT_API_TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+@dp.message_handler(commands="start")
+async def start(message: types.Message):
+    await message.answer(text='Choose a day')
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
