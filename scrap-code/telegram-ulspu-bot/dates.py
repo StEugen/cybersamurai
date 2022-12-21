cb02e4f15b3ad3c14b26fde9721cf4cc07892d7e
StEugen <85479518+StEugen@users.noreply.github.com>
2022-12-21 01:33:23 +0000

    update

--- scrap-code/telegram-ulspu-bot/dates.py
+++ scrap-code/telegram-ulspu-bot/dates.py
@@ -1,9 +1,12 @@
 from telebot import types 
 import re
+from test_json import get_dates
 
+get_dates()
 with open('files/dates', 'r') as f:
     dates = [line.strip() for line in f]
 
 keyboard = types.ReplyKeyboardMarkup(row_width=1)
 for item in dates:
     keyboard.add(item)
+
--- scrap-code/telegram-ulspu-bot/test_json.py
+++ scrap-code/telegram-ulspu-bot/test_json.py
@@ -1,15 +1,23 @@
 import json, pprint, re
 from urllib.request import Request, urlopen
-from dates import dates
 
+def teacher_json():
+    user_input = input()
+    req = Request(
+        url='https://www.ulspu.ru/students/schedule/prepod/groups-15.json',
+        headers={'User-Agent': 'Mozila/5.0'}
+    )
+    site = urlopen(req)
+    data = json.load(site)
+    
 
 def get_json(text):
     """ takes url for json and generates readable text """
     f = open('files/test', 'w')
     id = f'https://www.ulspu.ru/students/schedule/prepod/cb4ba39a83855bae2c56ff82453c7511-'
-    item = text[:-3]
+    #item = text[:-3]
     req = Request(
-        url=id+f'{item}.json',
+        url=id+f'{text}.json',
         headers={'User-Agent': 'Mozila/5.0'})
     site = urlopen(req)
     data = json.load(site)
@@ -25,3 +33,16 @@ def take_info():
     text = f.read()
     return text
         
+def get_dates():
+    """ Write dates, so dates.py can work with them """
+    f = open('files/dates', 'w')
+    req = Request(
+        url='https://www.ulspu.ru/students/schedule/prepod/weeks.json',
+        headers={'User-Agent': 'Mozila/5.0'}
+    )
+    site = urlopen(req)
+    data = json.load(site)
+    for item in data:
+        f.write(item)
+        f.write('\n')
+
