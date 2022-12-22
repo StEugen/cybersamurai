ecd12decf420ca8773c63557814f98c120c1f49b
StEugen <85479518+StEugen@users.noreply.github.com>
2022-12-22 15:47:24 +0000

    Functionally complete but
    sometimes: 400. Description: Bad Request: message is too long

--- scrap-code/telegram-ulspu-bot/ulspu_bot.py
+++ scrap-code/telegram-ulspu-bot/ulspu_bot.py
@@ -3,22 +3,26 @@ from config import BOT_API_TOKEN
 from telebot import types
 from test_json import get_json, take_info, teacher_json
 from dates import *
-
+global sname 
 
 bot = telebot.TeleBot(BOT_API_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    msg = bot.reply_to(message, 'Choose a week', reply_markup=keyboard)
+    msg = bot.reply_to(message, 'Enter you surname: ') #, reply_markup=keyboard)
     bot.register_next_step_handler(msg, dates)
 
 @bot.message_handler(commands=['help'])
 def help(message):
     bot.send_message(message.chat.id, 'Help displayed')
 
-
 def dates(message):
-    sname = '' 
+    global sname  
+    sname = message.text
+    msg = bot.reply_to(message, "Choose week", reply_markup=keyboard)
+    bot.register_next_step_handler(msg, test)
+
+def test(message):
     week = message.text
     teacher_json(sname, week)
     get_json()
