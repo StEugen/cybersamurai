b27e198a95e113c5f726118898566acaca725ae8
StEugen <85479518+StEugen@users.noreply.github.com>
2022-12-26 11:42:19 +0000

    Update formating.py

--- scrap-code/telegram-ulspu-bot/formating.py
+++ scrap-code/telegram-ulspu-bot/formating.py
@@ -5,6 +5,12 @@ from datetime import datetime
 def parse():
     pass
 
+str1 =  "date: 2022-12-09"
+
 def main():
-    f = open('files/timetable', 'w')
+    f = open('files/timetable')
+    for line in f:
+        if line == str1:
+            print(str1)
     
+main()
