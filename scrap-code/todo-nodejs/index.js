3556e2aecd672c7c8896749ed3a23700ffea91f5
StEugen <85479518+StEugen@users.noreply.github.com>
2022-12-29 12:30:52 +0000

    update

--- scrap-code/todo-nodejs/index.js
+++ scrap-code/todo-nodejs/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 
 app.use("/static", express.static("public"))
+app.use(express.urlencoded({ extended: true }))
 app.set("view engine", "ejs")
 
 
@@ -9,6 +10,8 @@ app.get('/',(req,res) => {
     res.render('todo.ejs');
 })
 
-
+app.post('/',(req, res) =>{
+    console.log(req.body);
+})
 
 app.listen(3000, () => console.log("Server is running", "url: http://localhost:3000"))
