a405f6277a9f0a65f864754f87225a0615bf06c4
StEugen <85479518+StEugen@users.noreply.github.com>
2022-12-29 12:22:07 +0000

    update

--- scrap-code/todo-nodejs/index.js
+++ scrap-code/todo-nodejs/index.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const app = express()
 
+app.use("/static", express.static("public"))
 app.set("view engine", "ejs")
+
 
 app.get('/',(req,res) => {
     res.render('todo.ejs');
