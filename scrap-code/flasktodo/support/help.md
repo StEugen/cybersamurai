9fc34ceb5870f5728b1317350cbd5ab05b0d33d1
StEugen <85479518+StEugen@users.noreply.github.com>
2023-03-27 11:21:59 +0000

    Some changes

---
+++ scrap-code/flasktodo/support/help.md
@@ -0,0 +1,5 @@
+# Help file
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@<ip.address.db.host>/<name.of.db>'
+<br>
+mysql -h remote-server-ip-address -P port-number -u username -p
--- scrap-code/flasktodo/support/sqlquerry.sql
+++ scrap-code/flasktodo/support/sqlquerry.sql
@@ -1,4 +1,13 @@
+-- PostgreSQL
 CREATE TABLE todo(
 id SERIAL PRIMARY KEY,
 title VARCHAR(30) NOT NULL,
 content VARCHAR(255) NOT NULL);
+
+-- Mariadb
+CREATE TABLE todo (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  content VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
