44b4c9651cfc733a2f2b63adb54e47fae8dcaf9e
StEugen <85479518+StEugen@users.noreply.github.com>
2023-10-03 15:07:48 +0400

    xlsx2csv without interface for now

---
+++ scrap-code/xlsx2csvsimple/xlsx2csv.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import shutil
+import os
+
+try:
+    input_file = input('Please provide filename: ')
+    output_file = 'output.csv'
+
+    data = pd.read_excel(input_file)
+    data.to_csv(output_file, index=False, encoding='utf-8')
+
+    print(f'Conversion from {input_file} to {output_file} completed.')
+
+    input_file_path = 'output.csv'
+    output_file_path = 'utf8_encoded_file.csv'
+
+    with open(input_file_path, 'r', encoding='utf-8-sig') as input_file:
+        content = input_file.read()
+
+    with open(output_file_path, 'w', encoding='utf-8') as output_file:
+        output_file.write(content)
+
+    os.remove("./output.csv")
+    print(f"The file {input_file_path} has been encoded with UTF-8 and saved as {output_file_path}.")
+
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
+    input()
