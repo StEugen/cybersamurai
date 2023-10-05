8e53e9919cf3e146979a005b4c6dd6b8e80017be
StEugen <85479518+StEugen@users.noreply.github.com>
2023-10-05 16:05:26 +0400

    cleaning a bit

--- scrap-code/xlsx2csvsimple/ui/main.py
+++ scrap-code/xlsx2csvsimple/ui/main.py
@@ -70,28 +70,6 @@ class MainWindow(QMainWindow):
 
             if group and faculty:
                 script_template = '''
-$UserList=IMPORT-CSV "C:\\SCUD 2023\\scudADwork.csv"
-
-FOREACH ($Person in $UserList) {{
-    $Domain="@ULSPU.RU"
-    $FullName=$Person.cn
-    $Company=$Person.company
-    $Department=$Person.department
-    $Description=$Person.description
-    $givenName=$Person.givenName
-    $initials=$Person.initials
-    $I=$Person.City
-    $sAMAccountName=$Person.sAMAccountName
-    $sn=$Person.sn
-    $userPrincipalName=$Person.sAMAccountName+$Domain
-    $userPassword=$Person.userPassword
-    $expire=$null
-
-    NEW-ADUSER -PassThru -Path "OU={group}, OU={faculty}, OU=Students, DC=ULSPU,DC=RU" -Enabled $True -AccountExpirationDate $expire -ChangePasswordAtLogon $False -AccountPassword (ConvertTo-SecureString $userPassword -AsPlainText -Force) -CannotChangePassword $True -City $I -Company $Company -Department $Department -Description $Description -DisplayName $FullName -GivenName $givenName -Initials $initials -Name $FullName -SamAccountName $sAMAccountName -Surname $sn -Title $Description -UserPrincipalName $userPrincipalName
-
-    Set-ADAccountControl $sAMAccountName -PasswordNeverExpires $True
-}}
-pause
 '''
 
                 script_file_path = "script.ps1"
