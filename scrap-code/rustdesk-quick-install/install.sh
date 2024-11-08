603cf49deead22d6046fc836695813ad07ba8204
steugen <p.gnomme@gmail.com>
2024-11-08 15:11:29 +0400

    rustdesk quick install

---
+++ scrap-code/rustdesk-quick-install/install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+sudo apt update
+
+if ! command -v docker &>/dev/null; then
+  sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+  sudo apt update
+  sudo apt install -y docker-ce docker-ce-cli containerd.io
+fi
+
+if ! command -v docker-compose &>/dev/null; then
+  sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+fi
+
+mkdir -p ~/rustdesk-server/data
+
+cat <<EOF >~/rustdesk-server/docker-compose.yml
+version: '3'
+
+services:
+  hbbs:
+    container_name: hbbs
+    image: rustdesk/rustdesk-server:latest
+    command: hbbs
+    volumes:
+      - ./data:/root
+    network_mode: host
+    depends_on:
+      - hbbr
+    restart: always
+
+  hbbr:
+    container_name: hbbr
+    image: rustdesk/rustdesk-server:latest
+    command: hbbr
+    volumes:
+      - ./data:/root
+    network_mode: host
+    restart: always
+EOF
+
+cd ~/rustdesk-server
+sudo docker-compose up -d
